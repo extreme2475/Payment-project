997240d090f637681a3942adad44e6d52c1afcbd
Himanshu <rawanihimanshu15@gmail.com>
2026-01-13 18:56:13 +0530

    Added vercel.json to fix 404 error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ||"http://localhost:5000/api", // backend base URL
+  // Sirf ye ek line honi chahiye baseURL ke liye
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", 
 });
 
 api.interceptors.request.use((config) => {
